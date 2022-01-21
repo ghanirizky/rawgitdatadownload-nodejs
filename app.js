aef2aafd743c21a91f2bed18896bf904e8d4b068
ghani <ghani.rizkymail@gmail.com>
2022-01-21 14:36:23 +0700

    add config and gitignore files

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 const axios = require('axios');
 const fs = require("fs");
-const PATH_FILE = "E:/BINA NUSANTARA/SKRIPSI/DATASET"
-const arr = [
-    {
-        file_name: "time_series_covid19_confirmed_global.csv",
-        url : "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-    },
-    {
-        file_name: "time_series_covid19_deaths_global.csv",
-        url : "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-    },
-    {
-        file_name: "time_series_covid19_recovered_global.csv",
-        url : "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-    },
-    {
-        file_name: "time_series_covid19_vaccine_global.csv",
-        url : "https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv"
-    }
-]
+const {PATH_FILE, RAW_DATA} = require('./config')
+
 
 createFile = async(file_name, data) => {
     const create = await fs.writeFile (`${PATH_FILE}/${file_name}`, data, function(err) {
@@ -35,11 +18,11 @@ createFile = async(file_name, data) => {
 
 getData = () => {
     let i = 1
-    console.log(`Number of file downloaded : ${arr.length}`);
+    console.log(`Number of file downloaded : ${RAW_DATA.length}`);
     console.log(`File Path : ${PATH_FILE}`);
     console.log("============================================================");
 
-    arr.forEach(async(item) => {
+    RAW_DATA.forEach(async(item) => {
         const config = {
             method: 'GET',
             url: item.url,
@@ -51,4 +34,5 @@ getData = () => {
     })    
     console.log("============================================================");
 }
+
 getData()
---
+++ config.js
@@ -0,0 +1,23 @@
+//DIRECTORY OF DOWNLOADED FILE
+exports.PATH_FILE = "E:/BINA NUSANTARA/SKRIPSI/DATASET"
+
+//FILE_NAME can be anything, extension based on downloaded file
+exports.RAW_DATA  = [
+    {
+        file_name: "time_series_covid19_confirmed_global.csv",
+        url : "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    },
+    {
+        file_name: "time_series_covid19_deaths_global.csv",
+        url : "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    },
+    {
+        file_name: "time_series_covid19_recovered_global.csv",
+        url : "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+    },
+    {
+        file_name: "time_series_covid19_vaccine_global.csv",
+        url : "https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv"
+    }
+]
+
